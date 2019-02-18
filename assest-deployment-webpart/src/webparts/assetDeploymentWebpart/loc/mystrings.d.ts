2ea6adb4b2e3f6e1d9bebc93fdc3cbe9e342dff2
Jonathan Batselos <jbatselos@withum.com>
2019-02-18 11:29:51 -0500

    this is the first commit

---
+++ assest-deployment-webpart/src/webparts/assetDeploymentWebpart/loc/mystrings.d.ts
@@ -0,0 +1,10 @@
+declare interface IAssetDeploymentWebpartWebPartStrings {
+  PropertyPaneDescription: string;
+  BasicGroupName: string;
+  DescriptionFieldLabel: string;
+}
+
+declare module 'AssetDeploymentWebpartWebPartStrings' {
+  const strings: IAssetDeploymentWebpartWebPartStrings;
+  export = strings;
+}
